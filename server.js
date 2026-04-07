fab9a51289985cb4bbca943ea4244b2880d9ce7c
bakusdiomande-atgt <bakus.diomande@gmail.com>
2026-04-07 09:21:38 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express');
-const app = express();
+const cors    = require('cors');
+const path    = require('path');
+
+const app  = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('<h1>PRESFOR OK</h1>');
+app.use(cors());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.static(path.join(__dirname)));
+
+app.get('/api/ping', (req, res) => {
+  res.json({ ok: true, message: 'PRESFOR Cavally actif' });
 });
 
-app.listen(PORT, () => {
-  console.log('Port', PORT);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log('PRESFOR Cavally - Port ' + PORT);
 });
